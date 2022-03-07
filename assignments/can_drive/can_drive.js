25cf079852f459ef7e3561322d4e31948a3a56df
Brahmam-12 <brahmamanumukonda000@gmail.com>
2022-03-07 11:00:25 +0530

    playWithArray

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,16 +1,10 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
     if (hasDrivingLiscence){
-        if(isTired==false && isSober==true){
-            return  "You can drive"
-
-        }
-        else if(isSober==true){
-            return "You shouldn't drive"
-        }
-        else{
-            return "You shouldn't drive"
-        }
+        if(isTired==false && isSober==true) return  "You can drive"
+        else if(isSober==true) return "You shouldn't drive"
+        else return "You shouldn't drive"
+        
     }
     else{
     return "You cannot drive"
