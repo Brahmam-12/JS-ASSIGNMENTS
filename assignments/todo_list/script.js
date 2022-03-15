1cdfeb8cdb4753e581112b0a0c7de8709a34035c
Brahmam-12 <98756307+Brahmam-12@users.noreply.github.com>
2022-03-15 11:16:00 +0530

    todo-list

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,4 +1,4 @@
-let Input = document.querySelector('input');
+/*let Input = document.querySelector('input');
 let btn = document.querySelector('button');
 let content = document.querySelector('.todos');
 let count =-1
@@ -12,3 +12,35 @@ btn.addEventListener('click',function(){
         content.removeChild(paragraph);
     })
 })
+*/
+
+
+
+
+
+const compBtn = document.querySelector('button#comp');
+const pendBtn = document.querySelector('button#pend');
+async function fetchtodo(){
+    let res=await fetch('https://jsonplaceholder.typicode.com/todos');
+    let data=await res.json();
+    let output='';
+    compBtn.onclick = function(){
+    data.forEach(function(todo,index) {
+        if (todo.completed == true){
+        output+=`<p key=${todo.id}>
+                ${todo.title}
+                </p>`
+}})
+    document.querySelector('.todos').innerHTML=output;
+    }
+    pendBtn.onclick = function(){
+        data.forEach(function(todo,index) {
+ if (todo.completed == false){
+            output+=`<p key=${todo.id}>
+                    ${todo.title}
+                    </p>`
+    }})
+        document.querySelector('.todos').innerHTML=output;
+        }
+}
+    fetchtodo();
